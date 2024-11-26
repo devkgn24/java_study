98e7cd04e6e27a2e6491394ae9fee5f95bc8f513
devkgn24 <devkgn24@gmail.com>
2024-11-26 12:29:20 +0900

    [kgn] 최초 commit
    	- 표준출력
    	- 이스케이프 문자

---
+++ lec01_basic/src/com/gn/practice/Introduce.java
@@ -0,0 +1,15 @@
+package com.gn.practice;
+
+public class Introduce {
+	public static void main(String[] args) {
+//		안녕하세요~
+//		저는 "김철수"입니다.
+//		나이는 20살이고, mbti는 istp입니다.
+		// 1. print만 사용
+		System.out.print("안녕하세요~\n");
+		System.out.print("저는 \"김철수\"입니다.\n");
+		System.out.print("나이는 20살이고, mbti는 istp입니다.");
+		
+		
+	}
+}
