4a521564e45d70cb75aed0380f27dc9d199ed498
devkgn24 <devkgn24@gmail.com>
2024-11-28 14:23:36 +0900

    [kgn] if문

---
+++ lec04_control/src/com/gn/study/ConditionStmt.java
@@ -0,0 +1,16 @@
+package com.gn.study;
+
+public class ConditionStmt {
+	public static void main(String[] args) {
+		// 1. if문
+		int number = 10;
+		if(number > 5) {
+			int su = 2; 
+			System.out.println("number는 5보다 큽니다.");
+			System.out.println(su);
+		}
+		// System.out.println(su);
+		
+	}
+
+}
