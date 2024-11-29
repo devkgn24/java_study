5459c8103ef1dce0676286c1856487056aa80257
devkgn24 <devkgn24@gmail.com>
2024-11-29 14:19:03 +0900

    [kgn] 중첩 for문

---
+++ lec04_control/src/com/gn/practice/Practice06.java
@@ -0,0 +1,15 @@
+package com.gn.practice;
+
+public class Practice06 {
+	public static void main(String[] args) {
+		int sum = 0;
+		for(int i = 1 ; ;i++) {
+			// sum = sum + i;
+			sum += i;
+			if(sum > 100) {
+				System.out.println("초과된 합 : "+sum);
+				break;
+			}
+		}
+	}
+}
--- lec04_control/src/com/gn/study/LoopStmt.java
+++ lec04_control/src/com/gn/study/LoopStmt.java
@@ -2,6 +2,7 @@ package com.gn.study;
 
 public class LoopStmt {
 	public static void main(String[] args) {
+		// 1. for문
 		// 1 ~ 10까지 숫자 모두 출력
 		// 초기식 -> 출발점(1)
 		// 조건식 -> 도착점(10)
@@ -11,5 +12,36 @@ public class LoopStmt {
 			System.out.println(i);
 		}
 //		System.out.println(i);
+		
+		// 2. 초기식 생략
+		int a = 1;
+		for(; a <= 5 ; a++) {
+			System.out.println(a);
+		}
+		
+		// 무한루프
+		// 3. 조건식 생략
+		for(int i = 1; ; i++) {
+			System.out.println(i);
+			if(i >= 10) {
+				break;
+			}
+		}
+		
+		// 4. 중첩 for문
+		// i는 1~2 까지 하나씩 커지면서 반복
+		// j는 1~3 까지 하나씩 커지면서 반복
+		// A B B B C A B B B C 
+		
+		for(int i = 1 ; i <= 2 ; i++) {
+			//System.out.print("A ");
+			for(int j = 1 ; j <= 3 ; j++) {
+				//System.out.print("B ");
+			}
+			//System.out.print("C ");
+		}
+
+				
+		
 	}
 }
