119f0a1a62803a0234197573b1595e4022e99ac9
devkgn24@gmail.com <devkgn24@gmail.com>
2025-04-09 11:28:01 +0900

    [김가남] 반복문 문제 만들기

---
+++ lec04_control/src/com/gn/practice/Practice12.java
@@ -0,0 +1,16 @@
+package com.gn.practice;
+
+public class Practice12 {
+	public static void main(String[] args) {
+//		for (int i = 1; i <= 9; i++) {
+//		    System.out.println("영희야 안녕!");
+//		}
+		for (int i = 1; i <= 2; i++) {
+		    System.out.println("철수가 "+i+"번째 그룹에게 인사합니다.");
+		    for (int j = 1; j <= 3; j++) {
+		        System.out.println("    → 친구 " + j + " : 안녕!");
+		    }
+		}
+
+	}
+}
